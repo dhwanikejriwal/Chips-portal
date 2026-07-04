f93879439526a54d40ffed02767b0c1d9bf48ce9
Priyanshu-Dewangan <151838932+Priyanshu-Dewangan@users.noreply.github.com>
2026-07-04 12:15:59 +0530

    Commit MAIL_FROM fallback in email_utils.py

--- backend/utils/email_utils.py
+++ backend/utils/email_utils.py
@@ -9,7 +9,7 @@ load_dotenv()
 conf = ConnectionConfig(
     MAIL_USERNAME = os.getenv('MAIL_USERNAME', ''),
     MAIL_PASSWORD = os.getenv('MAIL_PASSWORD', ''),
-    MAIL_FROM = os.getenv('MAIL_FROM', ''),
+    MAIL_FROM = os.getenv('MAIL_FROM') or 'noreply@chips-portal.in',
     MAIL_PORT = int(os.getenv('MAIL_PORT', 587)),
     MAIL_SERVER = os.getenv('MAIL_SERVER', ''),
     MAIL_FROM_NAME = os.getenv('MAIL_FROM_NAME', 'CHiPS Admin Portal'),
